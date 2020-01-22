cbc05002785301d2faf70c40b22102c2263ea5db
Fernanda24G <fernanda24gleznav@gmail.com>
2020-01-22 12:59:52 -0700

    Hola mundo

---
+++ Clase_1/Practica_1/program.cs
@@ -0,0 +1,14 @@
+using System;
+// importamos la libreria pricipal
+
+namespace practica_1
+{ 
+   class Program
+    {
+        static void Main(string args)
+        {
+            Console.WriteLine("Hola mundo");
+        }
+    }
+
+}
