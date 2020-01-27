37f737bb6d628510bb8342e94f14b3bef8d7edd7
Fernanda24G <fernanda24gleznav@gmail.com>
2020-01-27 12:31:13 -0700

    practica

---
+++ practicauno/practicauno/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+// importamos la libreria pricipal
+
+namespace practica_1
+{
+    class Program
+    {
+        //variable de la clase
+         static string nombre = "Fernanda";
+
+        static void Main(string[] args)
+        {
+            //Respondemos con el primer argumento 
+            // Condicion se debe de cumplir todas las sentencias si utilizamos &&
+            // Condicion se debe de cumplir cualquier de las sentencias si utilizamos ||
+            // Tipos de comparaciones > < == >= <= !=
+            // false is true
+            if (args[0].Length > 0 && args[0] == nombre) // true o false
+            {
+                Console.WriteLine($"Hola {args[0]}");
+            }
+            else
+            {
+                Console.WriteLine("usuario no permitido");
+            }
+        }
+    }
+}
