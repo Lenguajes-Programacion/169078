2b9b96eec28ec2350060d467adb417922a83c3f4
Fernanda24G <fernanda24gleznav@gmail.com>
2020-02-07 12:54:34 -0700

    practica2

---
+++ Practica2/Practica2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Practica2
+{
+    class Program
+    {
+        //Proyecto calculadora
+        //Se espera recibir los argumentos valor1 (int) valor2 (string)
+        //valor3 (int)
+        //responder resultado en (int)
+        static void Main(string[] args)
+        {
+            switch (args[1])
+            {
+                case "+":
+                    Console.WriteLine("Es una suma");
+                    break;
+
+                case "-":
+                    Console.WriteLine("Es una resta");
+                    break;
+
+                case "*":
+                    Console.WriteLine("Es una multiplicacion");
+                    break;
+
+                case "/":
+                    Console.WriteLine("Es una divicion");
+                    break;
+
+                default:
+                    Console.WriteLine("Los argumentos no cuentan con un operador valido" + args[1]);
+                    break;
+            }
+        }
+    }
+}
