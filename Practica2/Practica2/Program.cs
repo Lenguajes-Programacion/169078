3db3883056c947c6a76bdf751a21af5c38897cef
Fernanda24G <fernanda24gleznav@gmail.com>
2020-02-10 13:00:50 -0700

    Calculadora

--- Practica2/Practica2/Program.cs
+++ Practica2/Practica2/Program.cs
@@ -1,37 +1,51 @@
 ﻿using System;
-
-namespace Practica2
+namespace calculadora1
 {
     class Program
     {
-        //Proyecto calculadora
-        //Se espera recibir los argumentos valor1 (int) valor2 (string)
-        //valor3 (int)
-        //responder resultado en (int)
         static void Main(string[] args)
         {
-            switch (args[1])
+            string operadorES;
+            int Suma, Resta, Multiplicacion, Variable1, Variable2;
+            Console.WriteLine("Realiza una operacion");
+            Console.WriteLine("Escribe el primer digito");
+            Variable1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Escribe un operador");
+            operadorES = Console.ReadLine();
+            Console.WriteLine("Escribe el segundo digito");
+            Variable2 = Int32.Parse(Console.ReadLine());
+
+            Suma = Variable1 + Variable2;
+            Resta = Variable1 - Variable2;
+            Multiplicacion = Variable1 * Variable2;
+            double Division = (Variable1 / Variable2);
+
+
+            switch (operadorES)
             {
-                case "+":
-                    Console.WriteLine("Es una suma");
+                case "+" :
+                    Console.WriteLine("El resultado de tu suma es: " + Suma);
                     break;
 
                 case "-":
-                    Console.WriteLine("Es una resta");
+                    Console.WriteLine("El resultado de tu resta es: " + Resta);
                     break;
 
                 case "*":
-                    Console.WriteLine("Es una multiplicacion");
+                    Console.WriteLine("El resultado de tu multiplicacion es: " + Multiplicacion);
                     break;
 
                 case "/":
-                    Console.WriteLine("Es una divicion");
+                    Console.WriteLine("El resultado de tu division es: " + Division);
                     break;
 
                 default:
-                    Console.WriteLine("Los argumentos no cuentan con un operador valido" + args[1]);
+                    Console.WriteLine("Intentalo de nuevo " + operadorES);
                     break;
+
+
             }
+            Console.ReadLine();
         }
     }
 }
